23a2af1fa55d7ce5934404d6e7dec0056febc8ff
kthorn11 <kaitmarie713@gmail.com>
2026-01-26 09:48:46 -0800

    Add files via upload

---
+++ Road Trip Fuel Costs.cpp
@@ -0,0 +1,44 @@
+// Road Trip Fuel Costs.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int size = 5;
+    int cost[size];
+    
+    for (int index = 0; index < size; index++)
+    {
+        cin >> cost[index];
+    }
+    cout << "Costs: ";
+    for (int index = 0; index < size; index++)
+    {
+        cout << cost[index] << " ";
+    }
+    cout << endl;
+
+    int max_cost = cost[0];
+    for (int index = 1; index < size; index++)
+    {
+        if (cost[index] > max_cost)
+            max_cost = cost[index];
+    }
+
+    cout << "Highest cost: " << max_cost << endl;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
